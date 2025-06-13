462cd0db6656c6ec998205581a4befc8c53f2950
TerryAdrop <terry@adrop.io>
2025-06-13 16:26:23 +0900

    mongoose 설치
    conntectDB

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import { SpotifyExample } from '@/components/spotify-example'
 import { Suspense } from 'react'
+import { connectDB } from '@/libs/api-server/mongoose'
 
 
 export default function Home() {
     
     // const { data, isLoading } = useSpotifyToken()
-    
+    connectDB()
     return (
         <div className=''>
             <Suspense fallback={<div>Loading...</div>}>
