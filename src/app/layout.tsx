44b84b185f452eb628e872b874fd021265d00f00
Seungmo Ku <sungmo5845_@naver.com>
2025-06-20 23:09:19 +0900

    모바일 사파리 2중 스크롤 수정
    모바일 사파리 2중 스크롤 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@
 
 import { Quicksand } from 'next/font/google'
 import './globals.css'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { QueryClient } from '@tanstack/query-core'
 import { QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
@@ -21,13 +21,21 @@ export default function RootLayout({
 }>) {
     const [queryClient] = useState(() => new QueryClient())
     
+    function setViewportHeight() {
+        const vh = window.innerHeight * 0.01
+        document.documentElement.style.setProperty('--vh', `${vh}px`)
+    }
+    
+    window.addEventListener('resize', setViewportHeight)
+    setViewportHeight()
+    
     return (
         <html lang='en'>
-        <body className={`${quicksand.variable} antialiased w-screen h-screen flex items-center justify-center`}>
+        <body className={`${quicksand.variable} antialiased w-screen flex items-center justify-center overflow-hidden`} style={{ height: 'calc(var(--vh, 1vh) * 100)' }}>
         <QueryClientProvider client={queryClient}>
-            <div className='flex w-full h-full max-w-[1440px] px-4 overflow-hidden'>
+            <div className='flex w-full h-full max-w-[1440px] md:px-4 p-1 overflow-hidden'>
                 <Navbar/>
-                <main className='flex-1 max-h-full p-5 overflow-hidden'>
+                <main className='flex-1 max-h-full md:p-5 overflow-hidden'>
                     {children}
                 </main>
             </div>
