2033255ca29ad38c129a73bd98878f1eb8666d08
Seungmo Ku <sungmo5845_@naver.com>
2025-06-20 23:12:27 +0900

    useEffect 삭제
    useEffect 삭제

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@
 
 import { Quicksand } from 'next/font/google'
 import './globals.css'
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import { QueryClient } from '@tanstack/query-core'
 import { QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
