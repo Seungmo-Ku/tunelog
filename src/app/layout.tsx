ea465ae17f52f71d7f2dd704e6e4795a4365d705
Seungmo Ku <sungmo5845_@naver.com>
2025-06-20 23:19:36 +0900

    useEffect 사용
    useEffect 사용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@
 
 import { Quicksand } from 'next/font/google'
 import './globals.css'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { QueryClient } from '@tanstack/query-core'
 import { QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
@@ -21,13 +21,16 @@ export default function RootLayout({
 }>) {
     const [queryClient] = useState(() => new QueryClient())
     
-    function setViewportHeight() {
-        const vh = window.innerHeight * 0.01
-        document.documentElement.style.setProperty('--vh', `${vh}px`)
-    }
-    
-    window.addEventListener('resize', setViewportHeight)
-    setViewportHeight()
+    useEffect(() => {
+        function setViewportHeight() {
+            const vh = window.innerHeight * 0.01
+            document.documentElement.style.setProperty('--vh', `${vh}px`)
+        }
+        
+        window.addEventListener('resize', setViewportHeight)
+        setViewportHeight()
+        return () => window.removeEventListener('resize', setViewportHeight)
+    }, [])
     
     return (
         <html lang='en'>
