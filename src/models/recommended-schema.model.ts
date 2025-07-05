1e943505f4928ba35b8ddbc0fda707e8b44ab657
Seungmo Ku <sungmo5845_@naver.com>
2025-07-05 15:27:41 +0900

    Set default dates to 0 in recommendedSchema
    Changed the default values for validUntil and validFrom fields in recommendedSchema from Date.now() to 0. This ensures that new documents will have these fields set to 0 unless specified otherwise.

--- src/models/recommended-schema.model.ts
+++ src/models/recommended-schema.model.ts
@@ -4,8 +4,8 @@ import mongoose from 'mongoose'
 const recommendedSchema = new mongoose.Schema({
     type: { type: String, enum: ['album', 'artist', 'track', 'journal'], required: true },
     spotifyId: { type: String, required: true },
-    validUntil: { type: Date, default: Date.now() },
-    validFrom: { type: Date, default: Date.now() },
+    validUntil: { type: Date, default: 0 },
+    validFrom: { type: Date, default: 0 },
     deleted: { type: Boolean, default: false }
 }, { timestamps: true })
 
