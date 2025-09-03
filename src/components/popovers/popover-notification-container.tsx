32f66b56595cf2610923fdfb8b563788ddf72cbf
parkjs101 <trckstr4422@gmail.com>
2025-09-03 17:38:14 +0900

    Refactor popover-notification

--- src/components/popovers/popover-notification-container.tsx
+++ src/components/popovers/popover-notification-container.tsx
@@ -2,29 +2,22 @@
 
 import { PopoverNotification } from '@/components/popovers/popover-notification'
 import { useGetNotify } from '@/hooks/use-account'
-import { useEffect, useMemo } from 'react'
-import { atom, useAtom } from 'jotai'
+import { useMemo, useState } from 'react'
+import { INotify } from '@/libs/interfaces/account.interface'
 
-
-export const NotifyLengthAtom = atom(0)
 
 export const PopoverNotificationContainer = () => {
     const { data: notify, isLoading: isAccountLoading } = useGetNotify()
-    const [arrayLength, setArrayLength] = useAtom(NotifyLengthAtom)
+    const [array, setArray] = useState<INotify[]>([])
     
-    const notifyArray = useMemo(() => {
+    useMemo(() => {
         if (isAccountLoading || notify === undefined || notify === null) {
-            return []
         } else {
-            return notify
+            setArray(notify)
         }
-    }, [notify, isAccountLoading])
+    }, [notify, isAccountLoading, setArray])
     
-    useEffect(() => {
-        setArrayLength(notifyArray.length)
-    }, [setArrayLength, notifyArray])
-    
-    if (arrayLength === 0) {
+    if (array.length === 0 || isAccountLoading) {
         return (
             <div>
                 {isAccountLoading ? <div>Loading notification...</div> : <div>There is no new notification</div>}
@@ -33,9 +26,9 @@ export const PopoverNotificationContainer = () => {
     } else return (
         <div className='w-80 min-h-10 max-h-35 overflow-y-scroll'>
             {
-                notifyArray.map((notification, index) => {
+                array.map((notification, index) => {
                     return (
-                        <PopoverNotification notification={notification} key={index}/>
+                        <PopoverNotification notification={notification} key={index} array={array} setArray={setArray}/>
                     )
                 })
             }
--- src/components/popovers/popover-notification.tsx
+++ src/components/popovers/popover-notification.tsx
@@ -1,49 +1,43 @@
 import { Notify } from '@/libs/interfaces/account.interface'
 import { useRouter } from 'next/navigation'
 import { useTranslation } from 'react-i18next'
-import { useState } from 'react'
 import { X } from 'lucide-react'
-import { useAtom } from 'jotai/index'
-import { NotifyLengthAtom } from '@/components/popovers/popover-notification-container'
 import { useCheckNotify } from '@/hooks/use-account'
 
 
 interface NotificationProps {
     notification: Notify
+    array: Notify[]
+    setArray: (array: Notify[]) => void
 }
 
 export const PopoverNotification = ({
-        notification
+        notification, array, setArray
     }: NotificationProps
 ) => {
     
-    const [popover, setPopover] = useState(true) //새로고침 전에 화면에서 사라지게함
-    const [arrayLength, setArrayLength] = useAtom(NotifyLengthAtom) //새로고침 전에 화면에 no new notification 띄움
     const appRouter = useRouter()
     const { t } = useTranslation()
     const { mutateAsync: checkNotify } = useCheckNotify(notification._id)
     
     const handleUpdate = async () => {
         await checkNotify()
-        setPopover(false)
-        setArrayLength(arrayLength - 1)
+        setArray(array.filter(item => item._id !== notification._id))
     }
     
-    if (popover) {
-        return (
-            <div className='flex'>
-                <div className='w-max-80 overflow-y-scroll mb-2 hover: cursor-pointer hover:bg-gray-100 border-b-1'
-                     onClick={() => {
-                         if (notification.link !== undefined) appRouter.push(notification.link)
-                         handleUpdate()
-                     }}>
-                    {t(notification.info, { name: notification.name, type: notification.type })}
-                </div>
-                <X className='hover: cursor-pointer hover:bg-gray-200 text-red-500 size-6'
-                   onClick={() => {
-                       handleUpdate()
-                   }}/>
+    return (
+        <div className='flex'>
+            <div className='w-max-80 overflow-y-scroll mb-2 hover:cursor-pointer hover:bg-gray-100 border-b-1'
+                 onClick={() => {
+                     if (notification.link !== undefined) appRouter.push(notification.link)
+                     handleUpdate()
+                 }}>
+                {t(notification.info, { name: notification.name, type: notification.type })}
             </div>
-        )
-    } else return null
+            <X className='hover:cursor-pointer hover:bg-gray-200 text-red-500 size-6'
+               onClick={() => {
+                   handleUpdate()
+               }}/>
+        </div>
+    )
 }
